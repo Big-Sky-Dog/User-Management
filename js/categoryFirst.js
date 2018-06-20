9a8820072d0d1f5b4372b2838d0532ea3ea9d9d3
1 <1@1.com>
2018-06-20 14:45:24 +0800

    3

--- js/categoryFirst.js
+++ js/categoryFirst.js
@@ -1,14 +1,40 @@
 $(function () {
+  page = 1;
+  pageSize = 10;
+  totalPage = 0;
+  getData();
+  $('#prev').on('click', function () {
+    page--;
+    if (page < 1) {
+      page = 1;
+      alert('已经是第一页了');
+    }
+  })
+  $('#next').on('click', function () {
+    page++;
+    if (page > totalPage) {
+      page = totalPage;
+      alert('没有更多数据了')
+    }
+    getData();
+  })
+})
+function getData () {
   $.ajax({
     type: 'get',
     url: `${APP.A}/category/queryTopCategoryPaging`,
     data: {
-      page: 1,
-      pageSize: 10
+      page: page,
+      pageSize: pageSize
     },
     success: function (r) {
-      var html = template('categoryFirstTpl', r);
+      if (r.error) {
+        location.href = 'login.html';
+      } else {
+        var html = template('categoryFirstTpl', r);
       $('#categoryFirstBox').html(html);
+      }
+      totalPage = Math.ceil(r.total / pageSize);
     }
   })
-})
+}
